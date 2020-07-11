c538b0e384e10e27d868c610660788e88c93d798
amira_1991 <geomiro2011@gmail.com>
2020-07-11 12:54:19 +0200

    Updates

---
+++ precache-manifest.d6e756a098c765cb1835263bc93412d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88180f2fa19bb9cfa81bebb62445f1b6",
+    "url": "/Tactecs/index.html"
+  },
+  {
+    "revision": "f6d12f9dc22e511bc51b",
+    "url": "/Tactecs/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "252febe00af0306cb7d2",
+    "url": "/Tactecs/static/css/main.985a936e.chunk.css"
+  },
+  {
+    "revision": "f6d12f9dc22e511bc51b",
+    "url": "/Tactecs/static/js/2.f8f37e1a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Tactecs/static/js/2.f8f37e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252febe00af0306cb7d2",
+    "url": "/Tactecs/static/js/main.f849a5fa.chunk.js"
+  },
+  {
+    "revision": "3ac359c4fbc6eeeb5885",
+    "url": "/Tactecs/static/js/runtime-main.af7b3fc8.js"
+  },
+  {
+    "revision": "50b13dde6f80eaf8aa78206ab2e350f2",
+    "url": "/Tactecs/static/media/commitment.50b13dde.jpg"
+  },
+  {
+    "revision": "fd757fe8cc34fb1bf2ab51621d6a8005",
+    "url": "/Tactecs/static/media/knowledge.fd757fe8.jpg"
+  },
+  {
+    "revision": "b02a05f56d7b70e8ebe85c7f71253ac5",
+    "url": "/Tactecs/static/media/learning.b02a05f5.png"
+  },
+  {
+    "revision": "9ebdfb7f30ec8961f5a8261726e0168b",
+    "url": "/Tactecs/static/media/logo.9ebdfb7f.png"
+  },
+  {
+    "revision": "8b4860ffa282cddb6f535aa98d81cfff",
+    "url": "/Tactecs/static/media/relationship.8b4860ff.jpg"
+  }
+]);
